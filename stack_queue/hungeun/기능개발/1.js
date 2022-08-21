8d614466ff0d7879a9f49e4af659392704b51e0e
Hungeun2 <bepoohun@gmail.com>
2022-08-21 14:45:54 +0900

    stack 풀이수정 및 추가

--- stack_queue/hungeun/기능개발/1.js
+++ stack_queue/hungeun/기능개발/1.js
@@ -1,36 +1,23 @@
 function solution(progresses, speeds) {
-  var answer = [];
-  let count = 0;
+  let answer = [];
 
-  let reverse_progresses = progresses.map((x) => 100 - x);
+  while (progresses.length > 0) {
+    let count = 0;
 
-  let arr = reverse_progresses.map((x, i) =>
-    x % speeds[i] !== 0 ? parseInt(x / speeds[i]) + 1 : parseInt(x / speeds[i])
-  );
-
-  while (arr.length > 1) {
-    count = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (i === arr.length - 1) {
-        arr.shift();
-        count++;
-        answer.push(count);
-        break;
-      }
-      if (arr[i] < arr[i + 1]) {
-        arr.splice(0, i + 1);
-        count++;
-        answer.push(count);
-        break;
-      } else {
-        count++;
-      }
+    for (let i = 0; i < progresses.length; i++) {
+      progresses[i] += speeds[i];
     }
-  }
-  if (arr.length > 1) {
-    answer.push(1);
+
+    for (let i = 0; i < progresses.length; i++) {
+      if (progresses[i] < 100) break;
+      count += 1;
+    }
+
+    if (count > 0) {
+      answer.push(count);
+      progresses.splice(0, count);
+      speeds.splice(0, count);
+    }
   }
   return answer;
 }
-
-solution([93, 30, 55], [1, 30, 5]);
